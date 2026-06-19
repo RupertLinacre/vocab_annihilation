b10cb7c70518f8a8c67dcb9122b753353ac24e1d
Robin Linacre <robinlinacre@hotmail.com>
2026-06-19 21:15:02 +0100

    Add more Rupert vocabulary words

---
+++ vocab_definitions/rupert/chasm.ts
@@ -0,0 +1,7 @@
+export default {
+    word: 'chasm',
+    definition: 'A deep crack or gap in the ground.',
+    example: 'The explorers stopped at the edge of the chasm.',
+    synonyms: ['gorge', 'abyss'],
+    antonyms: ['bridge']
+};
---
+++ vocab_definitions/rupert/gargoyle.ts
@@ -0,0 +1,7 @@
+export default {
+    word: 'gargoyle',
+    definition: 'A carved stone creature on a building, often used as a waterspout.',
+    example: 'A gargoyle watched over the old cathedral roof.',
+    synonyms: ['stone monster', 'waterspout'],
+    antonyms: ['plain statue']
+};
